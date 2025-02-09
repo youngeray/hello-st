32fb11f5b1797e926c0c33900077c2179771af06
“youngeray” <youngeray@outlook.com>
2025-02-09 22:06:21 +0800

    example

--- app.py
+++ app.py
@@ -5,67 +5,23 @@ import numpy as np
 
 
 st.set_page_config(layout="wide")
+
 def index():
-    st.title("主页")
-
-    options = st.multiselect(
-        "What are your favorite colors",
-        ["Green", "Yellow", "Red", "Blue"],
-        ["Yellow", "Red"],
-    )
-
-    st.write("You selected:", options)
-
-    cols = st.columns(3)
-    with cols[0]:
-        ui.metric_card(title="Total Revenue", content="$45,231.89", description="+20.1% from last month", key="card1")
-    with cols[1]:
-        ui.metric_card(title="Total Revenue", content="$45,231.89", description="+20.1% from last month", key="card2")
-    with cols[2]:
-        ui.metric_card(title="Total Revenue", content="$45,231.89", description="+20.1% from last month", key="card3")
-
-    clicked = ui.button("Click", key="clk_btn",variant="outline")
-    st.write("UI Button Clicked:", clicked)
-
-    st.button("Click",type="primary")
-    st.button("Click",type="secondary",icon="🔥")
-
-    chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["河北", "山东", "河南"])
-
-    st.line_chart(chart_data,x_label="时间",y_label="温度")
-
-
-    uploaded_img = st.file_uploader("Choose a img")
-    if uploaded_img is not None:
-        # To read file as bytes:
-        bytes_data = uploaded_img.getvalue()
-        st.image(bytes_data)
-
-    uploaded_vd = st.file_uploader("Choose a video")
-    if uploaded_vd is not None:
-        # To read file as bytes:
-        bytes_data = uploaded_vd.getvalue()
-        st.video(bytes_data,format="video/mp4")
-
-
-    uploaded_ad = st.file_uploader("Choose a audio")
-    if uploaded_ad is not None:
-        # To read file as bytes:
-        bytes_data = uploaded_ad.getvalue()
-        st.audio(bytes_data,format="audio/mpeg")
-
-    with ui.card():
-        ui.element("input", key="nst2_input", label="Value")
-        ui.element("button", key="nst2_btn", text="Nest Submmit", variant="outline")
-
+    st.title("主页")    
+    # st.page_link("app.py",label="主页",icon="🏠")
+    st.page_link("mypages/hot.py",label="新闻热点",icon="📈")
+    st.page_link("mypages/bbcnews.py",label="BBC中文",icon="📰")
+    st.page_link("mypages/zuqiunews.py",label="足球新闻",icon="⚽")
+    st.page_link("mypages/weathers.py",label="24天气",icon="🌤️")
+    st.page_link("mypages/glmai.py",label="AI绘图",icon="🖼️")
 
 
 pages = {
     "首页":[st.Page(index,title="首页",icon="🏠")],
-    "数据分析":[st.Page('mypages/bi.py')],
     "新闻热点":[st.Page('mypages/hot.py',title="新闻热点",icon="🔥"),st.Page('mypages/bbcnews.py',title="BBC中文",icon="📰"),st.Page('mypages/zuqiunews.py',title="足球新闻",icon="⚽")],
     "24天气":[st.Page('mypages/weathers.py',title="天气查询",icon="🌤️")],
     "智能AI":[st.Page('mypages/glmai.py',title="AI绘图",icon="🖼️")],
+    "数据分析":[st.Page('mypages/bi.py',title="BI分析",icon="📊"),st.Page('mypages/example.py',title="例子",icon="🔥")]
 }
 
 pg = st.navigation(pages)
