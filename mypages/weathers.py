af30ac7b72ffe0df0e138b7fddb4913a16dd9c6a
“youngeray” <youngeray@outlook.com>
2025-03-11 22:38:18 +0800

    updata weather

--- mypages/weathers.py
+++ mypages/weathers.py
@@ -4,7 +4,7 @@ from scripts.weather import get_weather
 
 # st.set_page_config(page_title="新闻热点",page_icon="🔥",layout="wide")
 
-@st.cache_data(ttl=10)
+@st.cache_data(ttl=100)
 def get_weathers():
     weather_data = get_weather()
     return weather_data
@@ -17,13 +17,24 @@ weathers, = st.tabs(["24小时",])
 
 @st.fragment()
 def part1():
-    weathers.dataframe(
-        get_weathers(),
-        height=200,
-        width=600
-    )
 
-    weather_data = pd.DataFrame(get_weathers())
-    weathers.line_chart(weather_data,x="时间",y="温度")
+    data = get_weathers()["data"]
+    location = get_weathers()["city"]
+    weather_data = pd.DataFrame(data)
+
+
+    with weathers.container():
+        weathers.markdown(f'#### {location} 天气预报')
+        weathers.dataframe(
+            data,
+            height=200,
+            width=600
+        )
+
+
+        
+    with weathers.container():
+        weathers.markdown("---")
+        weathers.line_chart(weather_data,x="日期",y=["最高温度℃","最低温度℃"])
 
 part1()
--- scripts/weather.py
+++ scripts/weather.py
@@ -2,25 +2,28 @@ import requests
 import streamlit as st
 import json
 
+
 def get_weather():
-    url = st.secrets['weather_url']  # 替换为你的URL
-    headers = {
-        'Accept': 'application/json, text/plain, */*',
-        'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
-        'Connection': 'keep-alive',
-        'Cookie': 'HWWAFSESTIME=1737033279545; HWWAFSESID=2dc4979020cf649518; moji_setting=%7B%22internal_id%22%3A480%7D; liveview_page_cursor=eyJtaW5JZCI6ODE3NDkxNTQsIm1heElkIjo4MTczNjgyOCwibWluQ3JlYXRlVGltZSI6MTUwNzQyMTY2OTAwMCwibWF4Q3JlYXRlVGltZSI6MTUwNzE3Mzk3NDAwMH0%3D; PHPSESSID=qqjn057dqal029kfht5fae3si2',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0',
 
-    }
-    response = requests.get(url,headers=headers)
+    myipurl = st.secrets['myip_url']
+    response = requests.get(myipurl)
+    country = response.json()['network']['location']['country']
+    city = response.json()['network']['location']['city']
+    district = response.json()['network']['location']['district']
+    ip = response.json()['network']['ip']
+    location = f'{country}-{city}-{district}'
+    print(location,ip)
 
-    if response:
-        data = response.json()['hour24']
+    weatherurl = st.secrets['weather_url'] + district  # 替换为你的URL
+    wresponse = requests.get(weatherurl)
+
+    if wresponse:
+        data = wresponse.json()['data']['moji']['data']['forecast']
         # realtime_json = json.dumps(realtime,ensure_ascii=False, indent=4)
-        new_data = [{"日期":item["Fpredict_date"],"时间":f'{item["Fpredict_hour"]}:00', "温度": item["Ftemp"],"天气":item["Fcondition"]} for item in data]
+        new_data = [{"日期":item["date"],"白天":item["dayWeather"], "夜晚": item["nightWeather"],"最低温度℃":int(item["temperature"].replace('℃','').split('~')[0]),"最高温度℃":int(item["temperature"].replace('℃','').split('~')[1]),"湿度":item["humidity"],"白天风向":item["windDay"],"夜间风向":item["windNight"],"空气质量":item["airQuality"],} for item in data]
         # new_json = json.dumps(new_data, ensure_ascii=False, indent=4)
         # print(data)
-        return new_data
+        return {'city':location,'data':new_data}
 
     else:
         print("请求失败")
